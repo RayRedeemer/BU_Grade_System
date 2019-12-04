065d72833ac224ead2052ba03029c6247d6769e3
Xiankang Wu <jerrywxk@gmail.com>
2019-12-04 17:33:06 -0500

    modify AcademicObject class. Add index validation method isIndexValid() and comments.

--- src/backend/AcademicObject.java
+++ src/backend/AcademicObject.java
@@ -15,7 +15,7 @@ public abstract class AcademicObject {
     // 1. _parent and _descendants should not the same type as this
     // 2. _parent and _descendants should not be the same type as each other
     // 3. _descendants should be all the same type
-    // 4. _parent and _descendants should not have any typings that would be cyclical (going up parent or down
+    // 4. _parent and _descendants should not generating cyclical traversal(going up parent or down
     //      _descendants should eventually lead to a null reference
 
     public AcademicObject(int id, String name, String description, AcademicObject parent) {
@@ -68,27 +68,64 @@ public abstract class AcademicObject {
         _parent = destination;
     }
 
+    /**
+     * Get all descendants of the current object if it has.
+     *
+     * @return
+     */
     public ArrayList<AcademicObject> getAllDescendants() {
         if (this.hasDescendants()) return _descendants;
         return null;
     }
 
+    /**
+     * Get a single descendant if if has, or the index is valid.
+     *
+     * @param index
+     * @return AcademicObject instance
+     */
     public AcademicObject getDescendant(int index) {
-        if (this.hasDescendants()) return _descendants.get(index);
-        return null;
+        if (isIndexValid(index) && this.hasDescendants())
+            return _descendants.get(index);
+        else
+            return null;
     }
 
+    /**
+     * Remove a descendant given its index
+     *
+     * @param index
+     */
     public void removeDescendant(int index) {
-        if (this.hasDescendants() && index < _descendants.size()) _descendants.remove(index);
+        if (isIndexValid(index) && this.hasDescendants())
+            _descendants.remove(index);
     }
 
+    /**
+     * Check if the current object has decendants
+     *
+     * @return boolean.
+     */
     public boolean hasDescendants() {
         return _descendants != null;
     }
 
+    /**
+     * Add a single descendant to the current object
+     *
+     * @param ao descendant object to be added
+     */
     public void addDescendant(AcademicObject ao) {
         if (!this.hasDescendants()) _descendants = new ArrayList<AcademicObject>();
         _descendants.add(ao);
+    }
+
+    private boolean isIndexValid(int index) {
+        if (index < 0 || index >= _descendants.size()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
