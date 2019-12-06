8e2ed0566e6b47db0ea8c4c8ab3ad5f061e1f9f5
Xiankang Wu <jerrywxk@gmail.com>
2019-12-06 17:30:36 -0500

    SysPortal now has getResponse() for Request handling.

--- src/backend/SysPortal.java
+++ src/backend/SysPortal.java
@@ -3,10 +3,8 @@ package backend;
 import share.*;
 import db.*;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collection;
 
 /**
  * Class serves as the interface between GUI and backend. All APIs must be private
@@ -21,24 +19,14 @@ public class SysPortal implements Statisticsable {
         _head = head;
     }
 
-    // tried to add a generic method for creation of all responses.
-//    private <T> Response createResponse(Collection<T> collection, ) {
-//        Response res = new Response(head, true);
-//        for (Student student : getStudentList()) {
-//            res.addBody(student);
-//        }
-//        return res;
-//    }
-
     /**
      * Method that the frontend calls to retrieve responses by passing requests with corresponding RequestHead
      *
-     * @param req
-     * @return
+     * @param req Request obj sent from GUI. It has fields such as enum head to specify the operation
+     * @return res Response obj with head and status. Status will be true if succeeds, false otherwise.
      */
     public Response getResponse(Request req) {
         RequestHead head = req.getHead();
-        _currentObj = req.setCurrentObj();
         Response res = new Response(head, true);
         List<Object> params = req.getParams();
         List<Integer> ids = req.getIds();
@@ -46,11 +34,13 @@ public class SysPortal implements Statisticsable {
         setCurrentObj(ids);
 
         switch (head) {
+
             case LOGIN:
-                boolean isValid = login((String) params.get(0), (String) params.get(1));
+                Boolean isValid = login((String) params.get(0), (String) params.get(1));
                 return new Response(head, isValid);
             case LOGOUT:
                 return res;
+
             case GET_STUDENT_LIST:
                 for (Student student : getStudentListByCourse((Course) _currentObj)) {
                     res.addBody(student);
@@ -72,57 +62,64 @@ public class SysPortal implements Statisticsable {
                 }
                 return res;
             case GET_SUBMISSION_LIST:
-                for (Submission submission : getSubmissionList()) {
+                for (Submission submission : getSubmissionList((Assignment) _currentObj)) {
                     res.addBody(submission);
                 }
                 return res;
+
+            case ADD_INSTRUCTOR:
+                addInstructor((String) params.get(0), (String) params.get(1));
+                return res;
             case ADD_STUDENT:
-                DatabasePortal.getInstance().addStudent((Course) _currentObj, (String) params.get(0), (Boolean) params.get(1));
+                addStudent((String) params.get(0), (Boolean) params.get(1));
                 return res;
-            case ADD_COURSE:
-                DatabasePortal.getInstance().addStudent((Course) _currentObj, (String) params.get(0), (Boolean) params.get(1));
+            case DROP_STUDENT:
+                dropStudent((Integer) params.get(0));
                 return res;
-            case ADD_CATEGORY:
-                Category category = (Category) req.getFirstParam();
-                // todo store to db
-                return res;
-            case ADD_ASSIGNMENT:
-                Assignment assignment = (Assignment) req.getFirstParam();
-                // todo store to db
-                return res;
-            case ADD_SUBMISSION:
-                Submission submission = (Submission) req.getFirstParam();
-                // todo store to db
-                return res;
-            case REMOVE_STUDENT:
-                int studentKey = (int) req.getFirstParam();
-                // todo remove from db
-                return res;
-            case REMOVE_COURSE:
-                int courseKey = (int) req.getFirstParam();
-                // todo remove from db
-                return res;
-            case REMOVE_CATEGORY:
-                int categoryKey = (int) req.getFirstParam();
-                // todo remove from db
-                return res;
-            case REMOVE_ASSIGNMENT:
-                int assignmentKey = (int) req.getFirstParam();
-                // todo remove from db
-                return res;
-            case REMOVE_SUBMISSION:
-                int submissionKey = (int) req.getFirstParam();
-                // todo remove from db
+            case WITHDRAW_STUDENT:
+                withdrawStudent((Integer) params.get(0));
                 return res;
             case UPDATE_STUDENT:
+                updateStudent((Integer) params.get(0));
                 return res;
+
+            case ADD_COURSE:
+                addCourse((int) params.get(0), (String) params.get(1), (String) params.get(2), (String) params.get(3));
+                return res;
+            case ADD_CATEGORY:
+                addCategory((String) params.get(0), (String) params.get(1));
+                return res;
+            case ADD_ASSIGNMENT:
+                addAssignment((String) params.get(0), (String) params.get(1));
+                return res;
+            case ADD_SUBMISSION:
+                addSubmission((Integer) params.get(0), (Double) params.get(1), (Double) params.get(2), (Boolean) params.get(3));
+                return res;
+
+            case DELETE_COURSE:
+                deleteCourse((Integer) params.get(0));
+                return res;
+            case DELETE_CATEGORY:
+                deleteCategory((Integer) params.get(0));
+                return res;
+            case DELETE_ASSIGNMENT:
+                deleteAssignment((Integer) params.get(0));
+                return res;
+            case DELETE_SUBMISSION:
+                deleteSubmission((Integer) params.get(0));
+                return res;
+
             case UPDATE_COURSE:
+                updateCourse((Integer) params.get(0));
                 return res;
             case UPDATE_CATEGORY:
+                updateCategory((Integer) params.get(0));
                 return res;
             case UPDATE_ASSIGNMENT:
+                updateAssignment((Integer) params.get(0));
                 return res;
             case UPDATE_SUBMISSION:
+                updateSubmission((Integer) params.get(0));
                 return res;
         }
         return null;
@@ -131,9 +128,10 @@ public class SysPortal implements Statisticsable {
     /**
      * Track the current level of AcademicObject. If user is checking on a Category, then _currentObj will be updated
      * to that Category object.
+     *
      * @param ids
      */
-    private void setCurrentObj(ArrayList<Integer> ids) {
+    private void setCurrentObj(List<Integer> ids) {
         Course course = DatabasePortal.getInstance().getCourseById(ids.get(0));
         Category category = DatabasePortal.getInstance().getCategoryById(course, ids.get(1));
         if (category == null) {
@@ -143,10 +141,7 @@ public class SysPortal implements Statisticsable {
         if (assignment == null) {
             _currentObj = category;
         }
-        Submission submission = DatabasePortal.getInstance().getSubmissionById(assignment, ids.get(3));
-        if (submission == null) {
-            _currentObj = submission;
-        }
+        _currentObj = DatabasePortal.getInstance().getSubmissionById(assignment, ids.get(3));
     }
 
     /**
@@ -156,7 +151,7 @@ public class SysPortal implements Statisticsable {
      * @param password
      * @return
      */
-    private boolean login(String name, String password) {
+    private Boolean login(String name, String password) {
         DatabasePortal.getInstance().getInstructor(name, password);
         return true;
     }
@@ -164,51 +159,175 @@ public class SysPortal implements Statisticsable {
     /**
      * add a new student
      */
-    private Student addStudent(String name, boolean isGrad) {
+    private Student addStudent(String name, Boolean isGrad) {
         return DatabasePortal.getInstance().addStudent((Course) _currentObj, name, isGrad);
+    }
+
+    /**
+     * drop a student
+     *
+     * @param studentId
+     * @return
+     */
+    private Boolean dropStudent(int studentId) {
+        Student student = DatabasePortal.getInstance().getStudentById(studentId);
+        return DatabasePortal.getInstance().dropStudent(student);
+    }
+
+    /**
+     * withdraw a student
+     *
+     * @param studentId
+     * @return
+     */
+    private Boolean withdrawStudent(int studentId) {
+        Student student = DatabasePortal.getInstance().getStudentById(studentId);
+        return DatabasePortal.getInstance().withdrawStudent(student);
+    }
+
+    private Boolean updateStudent(int studentId) {
+        Student student = DatabasePortal.getInstance().getStudentById(studentId);
+        return DatabasePortal.getInstance().updateStudent(student);
     }
 
     /**
      * add a new instructor
      */
-    private void addInstructor(String name, String password) {
-        Instructor newInstructor = new Instructor(name, id, password);
-        // todo db add
+    private Instructor addInstructor(String name, String password) {
+        return DatabasePortal.getInstance().addInstructor(name, password);
     }
 
     /**
-     * add a new course. null if fails, Course obj if succeeds.
+     * add a new Course obj
+     *
+     * @param instructorId
+     * @param name
+     * @param description
+     * @param semester
+     * @return null if fails, Course obj if succeeds.
      */
     private Course addCourse(int instructorId, String name, String description, String semester) {
         return DatabasePortal.getInstance().addCourse(instructorId, name, description, semester);
     }
 
     /**
-     * add a new Category
+     * delete a course
+     * @param courseId
+     * @return
      */
-    private Category addCategory(int courseId, String name, String description) {
+    private Boolean deleteCourse(int courseId) {
         Course course = DatabasePortal.getInstance().getCourseById(courseId);
-        if (course == null) {
-            return null;
-        }
-        return DatabasePortal.getInstance().addCategory(course, name, description);
+        return DatabasePortal.getInstance().deleteCourse(course);
     }
 
     /**
-     * add a new Assignment
+     * update a course
+     * @param courseId
+     * @return
      */
-    private Assignment addAssignment(int categoryID, String name, String description) {
-
-        // todo get course from db
-        // todo add category obj to db
+    private Boolean updateCourse(int courseId) {
+        Course course = DatabasePortal.getInstance().getCourseById(courseId);
+        return DatabasePortal.getInstance().updateCourse(course);
     }
 
     /**
-     * add a new Submission
+     * add a new Category obj
+     *
+     * @param name
+     * @param description
+     * @return null if fails, Category obj if succeeds.
      */
-    private void addSubmission() {
-        // todo db add
+    private Category addCategory(String name, String description) {
+        return DatabasePortal.getInstance().addCategory((Course) _currentObj, name, description);
     }
+
+    /**
+     * delete a category
+     *
+     * @param categoryId
+     * @return
+     */
+    private Boolean deleteCategory(int categoryId) {
+        Category category = DatabasePortal.getInstance().getCategoryById((Course) _currentObj, categoryId);
+        return DatabasePortal.getInstance().deleteCategory(category);
+    }
+
+    /**
+     * update a category
+     * @param categoryId
+     * @return
+     */
+    private Boolean updateCategory(int categoryId) {
+        Category category = DatabasePortal.getInstance().getCategoryById((Course) _currentObj, categoryId);
+        return DatabasePortal.getInstance().updateCategory(category);
+    }
+
+    /**
+     * add a new Assignment obj
+     *
+     * @param name
+     * @param description
+     * @return null if fails, Assignment obj if succeeds.
+     */
+    private Assignment addAssignment(String name, String description) {
+        return DatabasePortal.getInstance().addAssignment((Category) _currentObj, name, description);
+    }
+
+    /**
+     * delete an assignment
+     * @param assignmentId
+     * @return
+     */
+    private Boolean deleteAssignment(int assignmentId) {
+        Assignment assignment = DatabasePortal.getInstance().getAssignmentById((Category)_currentObj, assignmentId);
+        return DatabasePortal.getInstance().deleteAssignment(assignment);
+    }
+
+    /**
+     * update an assignment
+     * @param assignmentId
+     * @return
+     */
+    private Boolean updateAssignment(int assignmentId) {
+        Assignment assignment = DatabasePortal.getInstance().getAssignmentById((Category)_currentObj, assignmentId);
+        return DatabasePortal.getInstance().updateAssignment(assignment);
+    }
+
+    /**
+     * add a new Submission obj
+     *
+     * @param studentId
+     * @param score
+     * @param bonus
+     * @param style
+     * @return null if fails, Assignment obj if succeeds.
+     */
+    private Submission addSubmission(int studentId, double score, double bonus, Boolean style) {
+        Student student = DatabasePortal.getInstance().getStudentById(studentId);
+        return DatabasePortal.getInstance().addSubmission((Assignment) _currentObj, student, score, bonus, style);
+    }
+
+    /**
+     * delete a submission
+     * @param submissionId
+     * @return
+     */
+    private Boolean deleteSubmission(int submissionId) {
+        Submission submission = DatabasePortal.getInstance().getSubmissionById((Assignment) _currentObj, submissionId);
+        return DatabasePortal.getInstance().deleteSubmission(submission);
+    }
+
+    /**
+     * update a submission
+     * @param submissionId
+     * @return
+     */
+    private Boolean updateSubmission(int submissionId) {
+        Submission submission = DatabasePortal.getInstance().getSubmissionById((Assignment) _currentObj, submissionId);
+        return DatabasePortal.getInstance().updateSubmission(submission);
+    }
+
+
 
     private ArrayList<Course> getCourseList() {
         return DatabasePortal.getInstance().getAllCourses();
