296bde01b0b901fee63fd3e8a9d1bfa2d44c5c3e
Xiankang Wu <jerrywxk@gmail.com>
2019-12-06 16:27:33 -0500

    Remove currentObj from Request since we do not need it

--- src/share/Request.java
+++ src/share/Request.java
@@ -9,53 +9,49 @@ import java.util.List;
 Author: Ziqi Tan
 */
 public class Request {
-	
-	private RequestHead head;
-	private List<Object> params;
-	private List<Integer> ids;
-	private AcademicObject currentObj;
-	
-	public Request(RequestHead _head) {
-		this.head = _head;
-		params = new ArrayList<Object>();		
-	}
-	
-	/**
-	 * getter()
-	 * */
-	public RequestHead getHead() {
-		return head;
-	}
-	
-	public List<Object> getParams() {
-		return params;
-	}
 
-	public Object getFirstParam() {
-		return params.get(0);
-	}
+    private RequestHead head;
+    private List<Object> params;
+    private List<Integer> ids;
 
-	public AcademicObject getCurrentObj() {
-		return this.currentObj;
-	}
+    public Request(RequestHead _head) {
+        this.head = _head;
+        params = new ArrayList<>();
+        ids = new ArrayList<>();
+    }
 
-	public List<Integer> getIds() {
-		return ids;
-	}
-	
-	/**
-	 * setter()
-	 * */
-	public void addParams(Object object) {
-		params.add(object);
-	}
-	
-	@Override
-	public String toString() {
-		String str = System.getProperty("line.separator") + "head: " + head + System.getProperty("line.separator");
-		for( int i = 0; i < params.size(); i++ ) {
-			str += "param " + i + ": " + params.get(i) + System.getProperty("line.separator");
-		}
-		return str;
-	}
+    /**
+     * getter()
+     */
+    public RequestHead getHead() {
+        return head;
+    }
+
+    public List<Object> getParams() {
+        return params;
+    }
+
+    public Object getFirstParam() {
+        return params.get(0);
+    }
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    /**
+     * setter()
+     */
+    public void addParams(Object object) {
+        params.add(object);
+    }
+
+    @Override
+    public String toString() {
+        String str = System.getProperty("line.separator") + "head: " + head + System.getProperty("line.separator");
+        for (int i = 0; i < params.size(); i++) {
+            str += "param " + i + ": " + params.get(i) + System.getProperty("line.separator");
+        }
+        return str;
+    }
 }
