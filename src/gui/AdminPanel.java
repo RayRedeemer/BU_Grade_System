e0326967e1c01162bd5fbf3391d9203ab48585a9
PhoenixTAN <tanziqi1756@outlook.com>
2019-12-07 15:12:59 -0500

    Add Course button updated. ActionListener has been added.

--- src/gui/AdminPanel.java
+++ src/gui/AdminPanel.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -14,10 +15,14 @@ import java.util.Arrays;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableColumnModel;
@@ -32,14 +37,15 @@ Author: Ziqi Tan
 */
 public class AdminPanel extends JPanel implements ActionListener {
 		
+	private JFrame addCourseFrame;
 	private JTable courseListTable;
 	private String[] columnNames = 
-		{ "Name", "Year", "Semester", "Enrollment Count", "Average Grade" };
+		{ "Name", "Semester", "Enrollment Count", "Average Grade" };
 	private String[][] data = { 
-            { "CS 591", "19", "Fall", "66", "--"}, 
-            { "CS 592", "19", "Fall", "30", "--" },
-            { "CS 666", "18", "Spring", "150", "83" },
-            { "CS 591", "17", "Summer", "111", "87"}
+            { "CS 591", "Fall19", "66", "--"}, 
+            { "CS 592", "Fall19", "30", "--" },
+            { "CS 666", "Spring18", "150", "83" },
+            { "CS 591", "Summer17", "111", "87"}
     }; 
 	private String[][] tableData;
 	
@@ -103,6 +109,7 @@ public class AdminPanel extends JPanel implements ActionListener {
         JButton addCourseButton = new JButton("Add Course");
         addCourseButton.setBounds(buttonX, buttonY, buttonWidth, textHeight);
 		add(addCourseButton);
+		addCourseButton.addActionListener(this);
 		
 		JButton cloneCourseButton = new JButton("Clone Course");
 		cloneCourseButton.setBounds(buttonX, buttonY + vGap, buttonWidth, textHeight);
@@ -183,12 +190,118 @@ public class AdminPanel extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		
+		if( event.getActionCommand().equals("Add Course") ) {
+			Request request = new Request(RequestHead.ADD_COURSE);
+			if( addCourseFrame != null ) {
+				addCourseFrame.dispose();
+			}
+			addCourseFrame = new Form();
+			
+		}
+		
+		if( event.getActionCommand().equals("Clone Course") ) {
+			
+		}
+				
 		if( event.getActionCommand().equals("Select Course") ) {
 			Request request = new Request(RequestHead.SELECT_COURSE);
 			request.addParams(selectedCourse);
 			FrontController.getInstance().dispatchRequest(request);
 		}
+		
+		if( event.getActionCommand().equals("Delete Course") ) {
+			
+		}
+		
+	}
+	
+	/**
+	 * inner class
+	 * form to submit and add a course
+	 * */
+	class Form extends JFrame {
+		
+		public Form() {
+			System.out.println("A form is creating.");
+			setTitle("Add course");	
+						
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			int frameWidth = (int)(screenSize.getWidth() * 0.36);
+			int frameHeight = (int)(screenSize.getHeight() * 0.6);
+			setSize(frameWidth, frameHeight);
+			      		
+			int hGap = 20;
+			int vGap = 50;
+			
+			setLocationRelativeTo(null);   // this will center your frame
+			
+			setVisible(true);
+			
+			JPanel formPanel = new JPanel();
+			formPanel.setLayout(null);
+			
+			int labelX = (int)(frameWidth*0.2);
+			int labelY = (int)(frameHeight*0.2);
+			
+			int labelWidth = 100;
+			int labelHeight = 25;
+			JLabel nameLabel = new JLabel("Course name: ");
+			nameLabel.setBounds(labelX, labelY, labelWidth, labelHeight);
+			formPanel.add(nameLabel);
+			
+			JLabel semesterLabel = new JLabel("Semester: ");
+			semesterLabel.setBounds(labelX, labelY + vGap, labelWidth, labelHeight);
+			formPanel.add(semesterLabel);
+			
+			JLabel despLabel = new JLabel("Description: ");
+			despLabel.setBounds(labelX, labelY + vGap * 2, labelWidth, labelHeight);
+			formPanel.add(despLabel);
+			
+			int textFieldWidth = 130;
+			JTextField nameField = new JTextField(20);
+			nameField.setBounds(labelX + nameLabel.getWidth() + hGap, nameLabel.getY(), textFieldWidth, labelHeight);
+			formPanel.add(nameField);
+			
+			JTextField semesterField = new JTextField(20);
+			semesterField.setBounds(labelX + semesterLabel.getWidth() + hGap, semesterLabel.getY(), textFieldWidth, labelHeight);
+			formPanel.add(semesterField);
+			
+			JTextArea despArea = new JTextArea();
+			despArea.setLineWrap(true);
+			JScrollPane textAreaScrollPane = new JScrollPane(despArea);
+			textAreaScrollPane.setBounds(labelX + despLabel.getWidth() + hGap, despLabel.getY(), (int) (textFieldWidth * 2.3), 180);
+			formPanel.add(textAreaScrollPane);
+			
+			int buttonWidth = 80;
+			JButton submit = new JButton("Submit");
+			submit.setBounds(labelX, labelY + vGap * 3, buttonWidth, labelHeight);
+			formPanel.add(submit);
+			submit.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					
+				}
+			});
+						
+			JButton cancel = new JButton("Cancel");
+			cancel.setBounds(labelX, labelY + vGap * 4, buttonWidth, labelHeight);
+			formPanel.add(cancel);
+			cancel.addActionListener(
+					new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent event) {
+							dispose();
+						}
+					}					
+			);
+			
+			
+			add(formPanel);
+		}
+		
+		
+		
 	}
 	
 }
