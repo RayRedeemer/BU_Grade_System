f29295ac21b629dbd61501d458d4207304e1598d
PhoenixTAN <tanziqi1756@outlook.com>
2019-12-12 19:19:14 -0500

    Update on CategoryPanel.java.

--- src/gui/CategoryPanel.java
+++ src/gui/CategoryPanel.java
@@ -27,11 +27,10 @@ import javax.swing.JEditorPane;
 
 public class CategoryPanel extends JPanel implements ActionListener {
 	
-	private JLabel lblTitle, lblWeight, lblDescription, lblComment, lblAssign;
-	private JButton btnLogout, btnSave, btnStatistics, btnReturn, btnAdd, btnUpdate;
-	private JEditorPane epComment;
+	private JLabel lblTitle, lblWeight, lblDescription, lblComment, lblAssign, lblAssignDesp, lblAssignComment;
+	private JButton btnLogout, btnAddAssign, btnUpdateAssign, btnDelAssign, btnReturn, btnUpdate;
+	private JEditorPane cateDespArea, cateComment, assignDesp, assignComment;
 	private JTable assignmentTable;
-	private JEditorPane despArea;
 	private JTextField weightField;
 	private JScrollPane scrollPane;
 	
@@ -52,10 +51,10 @@ public class CategoryPanel extends JPanel implements ActionListener {
 	private static final int headerHeight = 32;
 	
 	private String[] columnNames = { 
-		"ID", "Name", "Weight"
+		"ID", "Name", "Weight", "Assigned Date", "Due Date"
 	};
 	private String[][] data = { 
-        { "1", "TicTacToe", "5%" }
+        { "1", "TicTacToe", "5%", "LocalDateTime", "LocalDateTime"}
     }; 
 	
 	/**
@@ -93,6 +92,19 @@ public class CategoryPanel extends JPanel implements ActionListener {
 		lblTitle.setFont(titleFont);
 		add(lblTitle);
 		
+		btnLogout = new JButton("Logout");
+		btnLogout.setBounds(x + lblTitle.getWidth(), y, buttonWidth, textHeight);
+		add(btnLogout);
+		btnLogout.addActionListener(
+				new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent event) {
+						Request request = new Request(RequestHead.LOGOUT);
+						FrontController.getInstance().dispatchRequest(request);
+					}
+				}
+		);
+		
 		lblWeight = new JLabel("Weight: ");
 		lblWeight.setBounds(x, y + vGap, labelWidth, textHeight);
 		lblWeight.setFont(labelFont);
@@ -112,13 +124,22 @@ public class CategoryPanel extends JPanel implements ActionListener {
 		lblDescription.setFont(labelFont);
 		add(lblDescription);
 		
-		despArea = new JEditorPane();
-		despArea.setText(desp);
-		despArea.setBounds(x, lblDescription.getY() + lblDescription.getHeight(), (int)(frameWidth*0.35), 100);
-		add(despArea);
+		cateDespArea = new JEditorPane();
+		cateDespArea.setText(desp);
+		cateDespArea.setBounds(x, lblDescription.getY() + lblDescription.getHeight(), (int)(frameWidth*0.35), 100);
+		add(cateDespArea);
+				
+		lblComment = new JLabel("Comment:");
+		lblComment.setBounds(x + cateDespArea.getWidth() + 10, lblDescription.getY(), labelWidth, textHeight);
+		lblComment.setFont(labelFont);
+		add(lblComment);
+		
+		cateComment = new JEditorPane();
+		cateComment.setBounds(lblComment.getX(), lblComment.getY() + lblComment.getHeight(), (int)(frameWidth*0.35), 100);
+		add(cateComment);
 		
 		btnUpdate = new JButton("Update Category");
-		btnUpdate.setBounds(x, despArea.getY() + despArea.getHeight() + 10, buttonWidth * 2, textHeight);
+		btnUpdate.setBounds(x, cateDespArea.getY() + cateDespArea.getHeight() + 10, buttonWidth * 2, textHeight);
 		add(btnUpdate);
 				
 		lblAssign = new JLabel("Assignment:");
@@ -126,49 +147,61 @@ public class CategoryPanel extends JPanel implements ActionListener {
 		lblAssign.setFont(labelFont);
 		add(lblAssign);
 		
-		btnLogout = new JButton("Logout");
-		btnLogout.setBounds(x + lblTitle.getWidth(), y, buttonWidth, textHeight);
-		add(btnLogout);
-		btnLogout.addActionListener(
+		createJTable();
+		setScrollPane();
+		add(scrollPane);
+		
+		// TODO: add/update/delete assignment
+		int buttonX = scrollPane.getX() + scrollPane.getWidth() + hGap;
+		int buttonY = scrollPane.getY();
+		btnAddAssign = new JButton("Add Assignment");
+		btnAddAssign.setBounds(buttonX, buttonY, buttonWidth * 2, textHeight);
+		add(btnAddAssign);
+		
+		btnUpdateAssign = new JButton("Update Assignment");
+		btnUpdateAssign.setBounds(buttonX, buttonY + vGap, buttonWidth * 2, textHeight);
+		add(btnUpdateAssign);
+		
+		btnDelAssign = new JButton("Delete Assignment");
+		btnDelAssign.setBounds(buttonX, buttonY + vGap * 2, buttonWidth * 2, textHeight);
+		add(btnDelAssign);
+		
+		btnReturn = new JButton("Return");
+		btnReturn.setBounds(buttonX, buttonY + vGap * 3, buttonWidth * 2, textHeight);
+		add(btnReturn);
+		btnReturn.addActionListener(				
 				new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent event) {
-						Request request = new Request(RequestHead.LOGOUT);
-						FrontController.getInstance().dispatchRequest(request);
+						MainFrame.getInstance().removeCurPanel();
+						MainFrame.getInstance().setCoursePanel();
 					}
 				}
 		);
 		
-		btnSave = new JButton("Save Comment");
-		btnSave.setBounds(1030, 350, buttonWidth, textHeight);
-		add(btnSave);
+		lblAssignDesp = new JLabel("Description:");
+		lblAssignDesp.setFont(labelFont);
+		lblAssignDesp.setBounds(x, scrollPane.getY() + scrollPane.getHeight() + vGap, labelWidth, textHeight);
+		add(lblAssignDesp);
 		
-		btnStatistics = new JButton("Statistics");
-		btnStatistics.setBounds(1030, 477, buttonWidth, textHeight);
-		add(btnStatistics);
+		assignDesp = new JEditorPane();
+		assignDesp.setBounds(lblAssignDesp.getX(), lblAssignDesp.getY() + lblAssignDesp.getHeight(), (int)(frameWidth*0.35), 100);
+		add(assignDesp);
 		
-		btnReturn = new JButton("Return");
-		btnReturn.setBounds(1048, 683, buttonWidth, textHeight);
-		add(btnReturn);
-		btnReturn.addActionListener(this);
+		lblAssignComment = new JLabel("Comment:");
+		lblAssignComment.setFont(labelFont);
+		lblAssignComment.setBounds(x + assignDesp.getWidth() + 10, lblAssignDesp.getY(), labelWidth, textHeight);
+		add(lblAssignComment);
 		
-		lblComment = new JLabel("Comment:");
-		lblComment.setBounds(x, 572, labelWidth, textHeight);
-		lblComment.setFont(labelFont);
-		add(lblComment);
-		
-		epComment = new JEditorPane();
-		epComment.setBounds(x, lblComment.getY() + lblComment.getHeight(), 670, 100);
-		add(epComment);
-		
-		btnAdd = new JButton("Add Assignment");
-		btnAdd.setBounds(x, epComment.getY() + epComment.getHeight() + 10, buttonWidth * 2, textHeight);
-		add(btnAdd);
-		
-		createJTable();
-		setScrollPane();
-		add(scrollPane);
+		assignComment = new JEditorPane();
+		assignComment.setBounds(lblAssignComment.getX(), lblAssignComment.getY() + lblAssignComment.getHeight(), (int)(frameWidth*0.35), 100);
+		add(assignComment);
+			
 
+						
+		
+
+	
 	}
 	
 	private void createJTable() {
@@ -225,6 +258,7 @@ public class CategoryPanel extends JPanel implements ActionListener {
         	    
         	 }
         } );
+    
 	}
 
 	/**
@@ -235,7 +269,7 @@ public class CategoryPanel extends JPanel implements ActionListener {
 	    scrollPane = new JScrollPane(assignmentTable); 
 	    scrollPane.setBounds(lblAssign.getX(), lblAssign.getY() +lblAssign.getHeight(), 
 	    		tableWidth, 
-	    		100);
+	    		150);
 	}
 	
 	/**
@@ -255,11 +289,7 @@ public class CategoryPanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		if( event.getActionCommand().equals("Return") ) {
-			// Return to Admin Panel
-			MainFrame.getInstance().removeCurPanel();
-			MainFrame.getInstance().setCoursePanel();
-		}
+
 		
 	}
 }
